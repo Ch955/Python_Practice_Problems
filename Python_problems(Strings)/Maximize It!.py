e29eb1ea90211ec2e282c225997ead5640ea5be7
Ch955 <chakriedupulapati143@gmail.com>
2026-01-11 10:55:56 +0530

    Update Maximize It!.py

--- Python_problems(Strings)/Maximize It!.py
+++ Python_problems(Strings)/Maximize It!.py
@@ -5,12 +5,7 @@ for i in range(x):
     val = list(map(int,input().split()))
     l.append(val)
 val = 0
-for i in l:
-    max_value = max(i)
-    # print(max_value)
-    val+=(max_value)**2
-output = val%a
-print(output)
+
 for i in product(*l):
     val = max(val, sum(i)%a)
 print(val)
